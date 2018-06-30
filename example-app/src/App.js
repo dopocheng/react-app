1b6d6a95cf0313da685bb292b8bdb5f16f11d1f5
Pitcher <747015697@qq.com>
2018-06-30 21:25:36 +0800

    添加组件，切换不同界面
    1.安装 router4
    2.设置路由对应不同链接且点击高亮
    3.匹配对应路由显示 component(6.30)

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
+import { Route, NavLink} from 'react-router-dom';
+import './App.css'
 // import logo from './logo.svg';
 
+class lb extends Component {
+  render() {
+    return (
+        <div>
+            <h2>李白</h2>
+            <p>天门中断楚江开,碧水东流至此回</p>
+            <p>两岸青山相对出,孤帆一片日边来</p>
+        </div>
+    );
+  }
+}
+
+class df extends Component {
+  render() {
+    return (
+        <div>
+            <h2>杜甫</h2>
+            <p>天门中断楚江开,碧水东流至此回</p>
+            <p>两岸青山相对出,孤帆一片日边来</p>
+        </div>
+    );
+  }
+}
+
+class lsy extends Component {
+  render() {
+    return (
+        <div>
+            <h2>锦瑟</h2>
+            <ol>
+              <li>锦瑟无端五十弦，一弦一柱思华年</li>
+              <li>庄生晓梦迷蝴蝶，望帝春心托杜鹃</li>
+              <li>沧海月明珠有泪，蓝田日暖玉生烟</li>
+              <li>此情可待成追忆，只是当时已惘然</li>
+            </ol>
+        </div>
+    );
+  }
+}
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>*/}
         <h1>Simple SAP</h1>
         <ul className="header">
-          <li>李白</li>
-          <li>杜甫</li>
-          <li>李商隐</li>
+          <nav>
+            {/* NavLink 判断链接点击状态高亮显示 */}
+          <li><NavLink to="/" exact activeClassName="active">李白</NavLink></li>
+          <li><NavLink to="/df" activeClassName="active">杜甫</NavLink></li>
+          <li><NavLink to="/lsy" activeClassName="active">李商隐</NavLink></li>
+          </nav>
         </ul>
 
-        <div></div>
+        <div className="content">
+          {/* 和路由要匹配否则不显示component */}
+          <Route path="/" exact component={lb} />
+          <Route path="/df" component={df} />
+          <Route path="/lsy" component={lsy} />
+        </div>
       </div>
     );
   }
--- example-app/src/index.js
+++ example-app/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route} from 'react-router-dom'; 
+import { HashRouter,} from 'react-router-dom'; 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <HashRouter>
-        {/* 处理路由的逻辑,如果访问的是根目录就显示component指的组件 */}
-        <Route path="/" component={App} /> 
+       <App/>    
     </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
